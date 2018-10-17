f80f3d29c0da79f22ea0920879efefe584d8dde5
Yanela Pachacama Quispe <ypachacamaq@tecsup.edu.pe>
2018-10-17 12:43:07 -0500

    Initial Commit

---
+++ Lab09-Firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab09-Firebase
+//
+//  Created by Yanela Pachacama Quispe on 17/10/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
